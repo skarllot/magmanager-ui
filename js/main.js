8f56a0ff048f80160dc9dd76b4b93c35d74f666b
Fabrício Godoy <skarllot@gmail.com>
2015-07-25 19:08:02 -0300

    Initial skeleton for UI

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var app = angular.module('magmanager', [ 'ngRoute', 'ui.bootstrap' ]);
+
+app.config([ '$routeProvider', '$controllerProvider', function($routeProvider, $controllerProvider) {
+    // Lazy loading
+    app.registerCtrl = $controllerProvider.register;
+    
+    $routeProvider
+    .when('/', {
+        title: 'Home',
+        templateUrl: 'view/home.html'
+    })
+    .when('/vendor', {
+        title: 'Vendors',
+        templateUrl: 'view/vendor.html'
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+}]);
+
+app.run(['$location', '$rootScope', function($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
