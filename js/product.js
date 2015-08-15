1e587cc2262109e76980a973a8a20145673d573a
Fabrício Godoy <skarllot@gmail.com>
2015-08-15 00:07:26 -0300

    Small UI improvements for products view
    Added loading spinner.
    Swaped table columns to show name before technology.

--- js/product.js
+++ js/product.js
@@ -1,9 +1,12 @@
 angular.module('magmanager')
     .registerCtrl('productController', [ '$rootScope', '$scope', '$routeParams', 'vendorService', function($rootScope, $scope, $routeParams, vendorService) {
         $scope.vendor = {};
+        $scope.loaded = false;
+        
         vendorService.GetVendor($routeParams.id)
             .then(function(vendor) {
                 $rootScope.title = vendor.name;
                 $scope.vendor = vendor;
+                $scope.loaded = true;
             });
     }]);
