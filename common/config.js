15648fa803899034bce650ca7b173c0be8bc566f
Fabrício Godoy <skarllot@gmail.com>
2015-08-24 03:05:49 -0300

    Fixed vendor compare to include products

--- common/config.js
+++ common/config.js
@@ -9,7 +9,21 @@ define([ 'lodash' ], function(_) {
             return _.pick(obj1, keys);
         },
         
+        pickArrayFromObj: function(arr, obj2) {
+            return _.map(arr, function(item) {
+                return config.pickFromObj(item, obj2);
+            });
+        },
+        
         models: {
+            getProduct: function() {
+                return {
+                    id: '',
+                    name: '',
+                    technology: ''
+                };
+            },
+            
             getVendor: function() {
                 return {
                     id: '',
@@ -20,9 +34,14 @@ define([ 'lodash' ], function(_) {
             
             vendorEquals: function(vendor1, vendor2) {
                 var emptyVendor = this.getVendor();
+                var emptyProduct = this.getProduct();
 
                 vendor1 = config.pickFromObj(vendor1, emptyVendor);
                 vendor2 = config.pickFromObj(vendor2, emptyVendor);
+                vendor1.products = config.pickArrayFromObj(
+                    vendor1.products, emptyProduct);
+                vendor2.products = config.pickArrayFromObj(
+                    vendor2.products, emptyProduct);
 
                 return _.isEqual(vendor1, vendor2);
             }
